fcaf14f4545263df996e93398db95f28ff6e8461
Conor <brendanlane8901@gmail.com>
2024-05-26 20:56:19 -0500

    DOMLab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+document.getElementById('main');
+
+const element = document.createElement('h1');
+
+element.remove();
+
+let newHeader = document.createElement('h1');
+    newHeader.setAttribute("id", "victory");
+
+newHeader.innerHTML = "YOUR-NAME is the champion";
